0578f8fbaa11ec5576d071b05d537bca40d22713
joaosoutto <joaosoutto@hotmail.com>
2021-10-28 21:10:29 -0300

    starting project and initial configurations

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "../components/Heading";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Heading color="black" size="small" lineBottom lineColor="primary">
+        Wit Weather
+      </Heading>
+    </>
+  );
+};
+
+export default Home;
